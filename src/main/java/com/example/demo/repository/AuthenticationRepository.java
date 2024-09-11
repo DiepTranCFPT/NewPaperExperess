1edf2bbb87fe61f13001d3f8e3343b0326dde603
nhattruowng <ltn04098@gmail.com>
2024-09-11 18:01:24 +0700

    ad

--- src/main/java/com/example/demo/repository/AuthenticationRepository.java
+++ src/main/java/com/example/demo/repository/AuthenticationRepository.java
@@ -8,12 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
+
 
 
 public interface AuthenticationRepository extends JpaRepository<User, String>
-{   // dua ra daatabase
-
+{
      Optional<User> findByEmail(String email);
      boolean existsByEmail(String email);
      boolean existsByUid(String uid);
