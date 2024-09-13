721c025393493d215ea3fdde3a6b6df92bc4ff00
nhattruowng <ltn04098@gmail.com>
2024-09-13 14:15:01 +0700

    a

--- src/main/java/com/example/demo/repository/IArticleRepository.java
+++ src/main/java/com/example/demo/repository/IArticleRepository.java
@@ -6,10 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Optional;
 
 public interface IArticleRepository extends JpaRepository<Article,String> {
     List<Article> findByType(Type type);
-    Optional<Article> findByAuthor_NameContaining(@NotNull String author);
+    List<Article> findByAuthor_NameContaining(@NotNull String author);
     List<Article> findByContentContaining(@NotNull String content);
 }
